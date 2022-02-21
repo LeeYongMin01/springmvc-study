b940b7a5bbd44e1fb3e97e4a4205c6a997b1869e
LeeYongMin01 <vbn0213@gmail.com>
2022-02-21 14:21:52 +0900

    mvc study start

---
+++ src/main/java/com/hello/SpringmvcStudyApplication.java
@@ -0,0 +1,13 @@
+package com.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcStudyApplication.class, args);
+	}
+
+}
