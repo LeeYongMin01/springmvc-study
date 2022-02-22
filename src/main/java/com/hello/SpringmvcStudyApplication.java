de80d1b3a4821697c5826a29b395be78d6bede8f
LeeYongMin01 <vbn0213@gmail.com>
2022-02-22 14:11:36 +0900

    테스트

--- src/main/java/com/hello/SpringmvcStudyApplication.java
+++ src/main/java/com/hello/SpringmvcStudyApplication.java
@@ -2,11 +2,14 @@ package com.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
-public class SpringmvcStudyApplication {
-
-	public static void main(String[] args) {
+public class SpringmvcStudyApplication
+{
+	public static void main(String[] args)
+	{
 		SpringApplication.run(SpringmvcStudyApplication.class, args);
 	}
 
